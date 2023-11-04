198ce1fb9bd698648faeab13ab296ca91559a5c2
thanhnt612 <nguyentruongthanh0612@gmail.com>
2023-11-04 12:44:29 +0700

    save cors origin

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ app.use(express.json())
 app.use(cookieParser());
 app.use(cors(
   {origin: [
+    'https://traveldnd.netlify.app',
     'http://localhost:3000',
-    // 'https://traveldnd.netlify.app',
   ],
   credentials: true}
 ));
