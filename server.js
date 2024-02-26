87a74b80df52169dc1e642ddc66cc07044246c91
thanhnt612 <nguyentruongthanh0612@gmail.com>
2024-02-26 09:50:21 +0700

    fix::deploy server has blocked by CORS-9

--- server.js
+++ server.js
@@ -22,21 +22,16 @@ const URL = process.env.MONGO_DB;
 //Middleware
 app.use(express.json())
 app.use(cookieParser());
-// app.use(cors(
-//   {
-//     origin: ['https://traveldnd.netlify.app', 'http://localhost:3000'],
-//     credentials: true
-//   }
-// ));
-const corsOptions = {
-  origin: ['https://traveldnd.netlify.app', 'http://localhost:3000'],
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true
-};
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: ['https://traveldnd.netlify.app', 'http://localhost:3000'],
+    credentials: true
+  })
+);
+
 // app.use(morgan("dev"))
-app.use(helmet())
-app.use(compression())
+// app.use(helmet())
+// app.use(compression())
 app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ extended: true, limit: '30mb' }));
 
