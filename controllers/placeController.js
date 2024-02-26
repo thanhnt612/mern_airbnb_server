e838010097438b8b9636dac84f67590b15662209
thanhnt612 <nguyentruongthanh0612@gmail.com>
2024-02-26 15:00:03 +0700

    fix::has blocked by CORS-12

--- controllers/placeController.js
+++ controllers/placeController.js
@@ -42,6 +42,11 @@ export const getAllRoomController = async (req, res) => {
 
 export const checkStatusRoomController = async (req, res) => {
     try {
+        res.setHeader("Access-Control-Allow-Origin", "*")
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+        res.setHeader("Access-Control-Max-Age", "1800");
+        res.setHeader("Access-Control-Allow-Headers", "content-type");
+        res.setHeader("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS");
         const response = await checkStatusRoomService();
         return res.status(200).json(response);
     } catch (error) {
